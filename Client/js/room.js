385c716f7fb472ed666bc6f62ad38b6de90a926e
Jirka-Mayer <jirkamayer97@gmail.com>
2019-06-12 22:54:26 +0200

    CodeMirror

--- Client/js/room.js
+++ Client/js/room.js
@@ -1,1 +1,21 @@
-console.log("Hello room!")
+const CodeMirror = require("codemirror/lib/codemirror.js")
+require("codemirror/mode/javascript/javascript.js")
+require("codemirror/addon/edit/matchbrackets.js")
+require("codemirror/addon/edit/closebrackets.js")
+
+require("codemirror/lib/codemirror.css")
+
+let editor = CodeMirror(document.querySelector("#editor"), {
+    lineNumbers: true,
+    mode: { name: "javascript" },
+    indentUnit: 4,
+    indentWithTabs: false,
+    viewportMargin: Infinity,
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    showCursorWhenSelecting: true
+})
+
+editor.on("change", (instance, change) => {
+    console.log(change)
+})
